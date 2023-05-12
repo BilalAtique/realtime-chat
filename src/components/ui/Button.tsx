c447af524652297244132fa6ffcd096b8b6d2f9f
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2023-05-12 14:35:46 +0500

    Create Login page

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -31,7 +31,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, Va
 const Button: FC<ButtonProps> = ({ className, children, variant, isLoading, size, ...props }) => {
   return <button className='' disabled={isLoading} {...props}>
     {isLoading ? <Loader2 className='mr-2 h-4 animate-spin' /> : null}
-
+    {children}
   </button>
 }
 
