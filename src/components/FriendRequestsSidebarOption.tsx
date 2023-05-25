d38f5c8e122c60ad5ae00682994de34f3422c33d
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2023-05-25 18:39:07 +0500

    Solve lint errors and warning

--- src/components/FriendRequestsSidebarOption.tsx
+++ src/components/FriendRequestsSidebarOption.tsx
@@ -39,7 +39,7 @@ const FriendRequestsSidebarOption: FC<FriendRequestsSidebarOptionProps> = ({
                 friendRequestHandler
             );
         };
-    }, []);
+    }, [sessionId]);
 
     return (
         <Link
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -38,7 +38,7 @@ const Messages: FC<MessagesProps> = ({
             pusherClient.unsubscribe(toPusherKey(`chat:${chatId}`));
             pusherClient.unbind("incoming_message", messageHandler);
         };
-    }, []);
+    }, [chatId]);
 
     const formatTimestamp = (timestamp: number) => {
         return format(timestamp, "HH:mm");
